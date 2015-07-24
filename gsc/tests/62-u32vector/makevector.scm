ed0e4967a2e76db9e736989a0ec149cdacff2f6c
astlouisf <astlouisf@gmail.com>
2015-07-24 06:24:44 -0400

    Add u32vector primitives tests.

---
+++ gsc/tests/62-u32vector/makevector.scm
@@ -0,0 +1,27 @@
+(declare (extended-bindings) (not constant-fold) (not safe))
+
+(define f (##not 123))
+(define t (##not f))
+(define s "")
+(define x 1.5)
+(define y (##make-u32vector 2 65536))
+(define z (##list 1 2 3))
+
+(define (test x)
+  (println (##u32vector? x))
+  (println (if (##u32vector? x) 11 22)))
+
+(test 0)
+(test 1)
+(test f)
+(test t)
+(test s)
+(test x)
+(test y)
+(test z)
+(test (##cdr z))
+
+(println (##u32vector-ref y 0))
+(println (##u32vector-ref y 1))
+(##u32vector-set! y 1 88)
+(println (##u32vector-ref y 1))
